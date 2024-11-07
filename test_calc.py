923a10c2f86aeec1257c57f04bf798870a2da119
Eshani Nandy <47856506+eshaninandy@users.noreply.github.com>
2024-11-07 15:01:28 -0500

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -12,6 +12,15 @@ class TestCalculator(unittest.TestCase):
         self.calc.add_to_expression(3)
         self.calc.evaluate()
         self.assertEqual(self.calc.current_expression, '8')
+        self.calc.add_to_expression(15)
+        self.calc.append_operator('+')
+        self.calc.add_to_expression(30)
+        self.calc.append_operator('+')
+        self.calc.add_to_expression(405)
+        self.calc.append_operator('+')
+        self.calc.add_to_expression(300)
+        self.calc.evaluate()
+        self.assertEqual(self.calc.current_expression, '750')
 
     def test_subtraction(self):
         self.calc.add_to_expression(5)
@@ -33,6 +42,41 @@ class TestCalculator(unittest.TestCase):
         self.calc.add_to_expression(2)
         self.calc.evaluate()
         self.assertEqual(self.calc.current_expression, '4.0')
+        
+    def test_combined_operations(self):
+        # (5 + 3) * 2
+        self.calc.add_to_expression(5)
+        self.calc.append_operator('+')
+        self.calc.add_to_expression(3)
+        self.calc.append_operator('*')
+        self.calc.add_to_expression(2)
+        self.calc.evaluate()
+        self.assertEqual(self.calc.current_expression, '16')
+
+        # (10 - 2) / 4
+        self.calc.clear_expression()
+        self.calc.add_to_expression(10)
+        self.calc.append_operator('-')
+        self.calc.add_to_expression(2)
+        self.calc.append_operator('/')
+        self.calc.add_to_expression(4)
+        self.calc.evaluate()
+        self.assertEqual(self.calc.current_expression, '2.0')
+        
+    def test_more_complex_expression(self):
+        # 10 + 2 * 3 - 5 / 5
+        self.calc.clear_expression()
+        self.calc.add_to_expression(10)
+        self.calc.append_operator('+')
+        self.calc.add_to_expression(2)
+        self.calc.append_operator('*')
+        self.calc.add_to_expression(3)
+        self.calc.append_operator('-')
+        self.calc.add_to_expression(5)
+        self.calc.append_operator('/')
+        self.calc.add_to_expression(5)
+        self.calc.evaluate()
+        self.assertEqual(self.calc.current_expression, '16.0') 
 
 if __name__ == "__main__":
     unittest.main()
